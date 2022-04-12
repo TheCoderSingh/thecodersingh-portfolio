9f35dd934bc6cb8e4baaf6711ecde7f28d67c7f5
Jaskaran Singh <thecodesingh@gmail.com>
2022-04-11 18:05:57 -0700

    Create projects component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import Projects from "./components/Projects/Projects";
 
 const App = () => {
   return (
     <div className="container">
       <Header />
       <Home />
+      <Projects />
     </div>
   );
 };
